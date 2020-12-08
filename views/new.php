9d99d3030f441fdddc986f50c4198d610ba93195
yuzuyuzu0830 <kiki.balon@gmail.com>
2020-12-08 23:10:01 +0900

    bootstrap追加

--- views/new.php
+++ views/new.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.css">
     <title>読書ログ登録</title>
 </head>
 <body>
